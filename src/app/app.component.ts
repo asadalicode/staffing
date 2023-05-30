c0dc3ea27c07864932aaddb3626e64532145b9b7
Asad Ali <asadalicode123@gmail.com>
2023-05-30 20:46:20 +0500

    language fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { merge } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
 
+import { environment } from '@env/environment';
+import { I18nService } from '@app/i18n';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'staffing-app';
+
+  constructor(
+    private translateService: TranslateService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private i18nService: I18nService
+  ) {}
+
+  ngOnInit() {
+    // Setup translations
+    this.i18nService.init(
+      environment.defaultLanguage,
+      environment.supportedLanguages
+    );
+
+    const onNavigationEnd = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
+
+    // Change page title on navigation or language change, based on route data
+    merge(this.translateService.onLangChange, onNavigationEnd)
+      .pipe(
+        map(() => {
+          let route = this.activatedRoute;
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter((route) => route.outlet === 'primary'),
+        switchMap((route) => route.data),
+        untilDestroyed(this)
+      )
+      .subscribe((event) => {
+        // const title = event['title'];
+        // if (title) {
+        //   this.titleService.setTitle(this.translateService.instant(title));
+        // }
+      });
+  }
+
+  ngOnDestroy() {
+    this.i18nService.destroy();
+  }
 }
